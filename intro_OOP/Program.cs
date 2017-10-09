247c0cf8dc080c826beee53f6c8e16540b348d00
monicawp <monicawperezf@gmail.com>
2017-10-09 11:13:08 -0400

    OOP class notes

--- intro_OOP/Program.cs
+++ intro_OOP/Program.cs
@@ -10,6 +10,22 @@ namespace intro_OOP
     {
         static void Main(string[] args)
         {
+            cat mittens = new cat();
+            mittens.Name = "Mittens";
+
+            cat secondCat = new cat("Oz", 3, "orange and white");
+            Console.WriteLine(mittens.Name);
+            Console.WriteLine(secondCat.Name);
+
+
+            mittens.Eat();
+            
+            mittens.WorkOut(); 
+
+
+
+
+
         }
     }
 }
--- intro_OOP/cat.cs
+++ intro_OOP/cat.cs
@@ -8,5 +8,69 @@ namespace intro_OOP
 {
     class cat
     {
+        //states
+        //fields 
+        private string name;
+        private int age;
+        private string furColor;
+        private bool isHungry = true;
+
+
+
+        //properties
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; } 
+        } 
+
+        //behaviors
+
+        //constructors
+        public cat()
+        {
+            //default contructor because it takes no 
+            // parameters in the parentheses 
+        }
+
+        public cat(string name, int age, string furColor)
+        {
+            this.name = name;
+            this.age = age;
+            this.furColor = furColor; 
+        }
+
+        //methods
+
+        public void Eat()
+        {
+            if(isHungry = true)
+            {
+                isHungry = false;
+            }
+
+            Console.WriteLine("Is the cat hungry? " + isHungry); 
+        }
+
+        public void WorkOut()
+        {
+            if(isHungry == false)
+            {
+                isHungry = true;
+
+            }
+            Console.WriteLine("The cat worked out. Hungry now? " + isHungry); 
+        }
+
+
+
+
+
+
+
+
+
+
+
     }
 }
